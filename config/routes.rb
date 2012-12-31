eefdd73c06cb7e27865df7bbf310e7a759bf6889
Michael Stewart <mickle00@gmail.com>
2012-12-31 15:45:45 -0800

    defaulting app to goals page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Goals::Application.routes.draw do
   resources :logs
-
-
   resources :goals
+  root :to => 'goals#index'
 
 
   # The priority is based upon order of creation:
@@ -54,7 +53,6 @@ Goals::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
