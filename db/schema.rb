990f267a98b6bd42e3711e89d9ed1e50bb09ab91
Michael Stewart <mickle00@gmail.com>
2012-12-31 21:14:39 -0800

    schema changes and minor formatting

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121231090022) do
+ActiveRecord::Schema.define(:version => 20130101042402) do
 
   create_table "goals", :force => true do |t|
     t.string   "name"
     t.string   "frequency"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "count"
   end
 
   create_table "logs", :force => true do |t|
